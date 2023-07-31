418252f251ec742ddc49c1f397f519f883f8c7b8
AbhishekJavaDeveloper <ak1997jd@gmail.com>
2023-07-31 21:41:43 +0530

    DOM

--- dom.js
+++ dom.js
@@ -19,11 +19,34 @@
 // var header = document.getElementById('main-header');
 // header.style.borderBottom = 'solid 3px #000';
 
-var items = document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
+// var items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// items[2].style.backgroundColor='green';
+
+// for(let i=0;i<items.length;i++){
+//     items[i].style.fontWeight = 'bold';
+// }
+
+var li = document.getElementsByClassName('li');
+console.log(li);
+console.log(li[1]);
 items[2].style.backgroundColor='green';
 
 for(let i=0;i<items.length;i++){
     items[i].style.fontWeight = 'bold';
 }
+
+
+
+const itemsByClassName = document.getElementsByClassName("list-group-item");
+  if (itemsByClassName.length > 0) {
+    const newItemByClassName = itemsByClassName[itemsByClassName.length - 1]; 
+    newItemByClassName.textContent = "Edited by getElementsByClassName";
+  }
+
+  const itemsByTagName = document.getElementsByTagName("li");
+  if (itemsByTagName.length > 0) {
+    const newItemByTagName = itemsByTagName[itemsByTagName.length - 1]; 
+    newItemByTagName.textContent = "Edited by getElementByTagName";
+  }
